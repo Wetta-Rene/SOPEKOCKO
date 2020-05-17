29290412efe0b2cfff29d65b7cb1c67033fb83c0
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-05-17 14:40:29 +0200

    initial commit

---
+++ backend/routes/stuff.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config')
+
+
+router.get('/', auth, stuffCtrl.getAllSauces);
+router.post('/', auth, multer, stuffCtrl.createSauce);
+router.get('/:id', auth, stuffCtrl.getOneSauce);
+router.post('/:id/like', auth, stuffCtrl.likeSauce); //modify en likes
+
+module.exports = router;
