68fb19138bb4c7d8b55e0b9c8d49c8b078f0f8d3
Votre nom ou pseudo <63054356+Wetta-Rene@users.noreply.github.com>
2020-05-28 22:21:54 +0200

    no message

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -62,15 +62,20 @@ exports.likeOrDislike = (req, res, next) => {
         .then ((sauce)=> res.status(200).json({ message: '2-Like modifié !'}))
         .catch(error => res.status(400).json({ error }));
     } else{ //like vaut 0
-        if(Sauce.find({usersLiked: req.body.userId})){
-        Sauce.updateOne({_id: req.params.id}, {$pull: {usersLiked: req.body.userId}, $inc: {likes: -1}})
-             .then((sauce) => {res.status(200).json({ message: '3-Like modifié !'})}) 
+        
+        Sauce.findOne({_id: req.params.id})
+            .then(sauce => {
+                if (sauce.usersLiked.includes(req.body.userId)) {
+                    Sauce.updateOne({_id: req.params.id}, {$pull: {usersLiked: req.body.userId}, $inc: {likes: -1}})
+                    .then((sauce) => {res.status(200).json({ message: '3-Like modifié !'})}) 
+                    .catch(error => res.status(400).json({ error }))
+                } else if (sauce.usersDisliked.includes(req.body.userId)) {
+                    Sauce.updateOne({_id: req.params.id}, {$pull: {usersDisliked: req.body.userId}, $inc: {dislikes: -1}})
+                    .then((sauce) => {res.status(200).json({ message: '4-Like modifié !'})}) 
+                    .catch(error => res.status(400).json({ error }))  
+                }
+            })
             .catch(error => res.status(400).json({ error }));
-        }else{
-         Sauce.updateOne({_id: req.params.id}, {$pull: {usersDisliked: req.body.userId}, $inc: {dislikes: -1}})
-            .then((sauce) => {res.status(200).json({ message: '4-Like modifié !'})}) 
-            .catch(error => res.status(400).json({ error }));           
-        }
     }
 };
 
